d3422643806a8c02ed50066c9a9f5810e4f99dcf
Ethan Spiro & Samantha Stallings <pair+ethanspiro.peanutbutternsam@devbootcamp.com>
2014-10-30 15:30:39 -0700

    3rd commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,16 @@
 get '/' do
   # Look in app/views/index.erb
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+    erb :index
+  else
+  redirect '/login'
+  end
   erb :index
 end
 
 get '/login' do
-
+  erb :login
 end
 
 post '/login' do
@@ -12,10 +18,10 @@ post '/login' do
 end
 
 get 'profile/:id' do
-
+  erb :profile
 end
 
 get '/twit/:id' do
-
+  erb :twit
 end
 
