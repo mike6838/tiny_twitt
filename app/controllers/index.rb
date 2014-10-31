927a223fddaba94c532f0f08f801fe6d54fd51f7
Ethan Spiro & Samantha Stallings <pair+ethanspiro.peanutbutternsam@devbootcamp.com>
2014-10-30 18:00:22 -0700

    mvp

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,7 @@
 get '/' do
   # Look in app/views/index.erb
-  if session[:user_id] = 2
+  if session[:user_id]
+
     @current_user = User.find(session[:user_id])
 
     @twits_to_show = []
@@ -8,7 +9,6 @@ get '/' do
       @twits_to_show << followee.twits
     end
     @twits_to_show.flatten!
-    #remember to sort by date asc
     erb :index
   else
   redirect '/login'
@@ -16,28 +16,42 @@ get '/' do
 end
 
 get '/login' do
+  if session[:user_id]
+    redirect '/'
+  end
   erb :login
 end
 
 post '/login' do
-
+  session[:user_id] = User.find_by(:username => params[:username], :password => params[:password]).id
+  redirect '/'
 end
 
 post '/signup' do
-  User.create(username: params[:username],
+  u = User.create(username: params[:username],
               fname: params[:fname],
               lname: params[:lname],
               email: params[:email],
               password: params[:password])
-  @current_user = User.find(session[:user_id])
-  redirect 'profile/:id'
+  redirect '/login'
 end
 
-get 'profile/:id' do
+get '/profile/:id' do
+  @current_user = User.find(params[:id])
   erb :profile
 end
 
 get '/twit/:id' do
   erb :twit
+end
+
+post '/twit/:id' do
+  Twit.create(:content => params[:content], :user_id => params[:id])
+  redirect "/profile/#{params[:id].to_s}"
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,32 +8,4 @@ class User < ActiveRecord::Base
   # has_many :reverse_followings, foreign_key: :followee_id, class_name: 'Following'
   # has_many :followees, :through => :reverse_followings
 
-  def time_difference
-    seconds_since = (Time.now.to_i - self.created_at.to_i)
-    if seconds_since >= 86400 #seconds per day
-      return days(seconds_since)
-    elsif seconds_since >= 3600 #seconds per hour
-      return hours(seconds_since)
-    elsif seconds_since >= 60
-      return minutes(seconds_since)
-    else
-      return "#{seconds_since} seconds ago"
-    end
-  end
-
-  def days(seconds_since)
-    days = seconds_since / 3600 / 24
-    "#{days} days ago"
-  end
-
-  def hours(seconds_since)
-    hours = seconds_since / 3600
-    "#{hours} hours ago"
-  end
-
-  def minutes(seconds_since)
-    minutes = seconds_since / 60
-    "#{minutes} minutes ago"
-  end
-
 end
