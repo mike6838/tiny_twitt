7be18434e13bbb2d61f925cdc482a0cad60de3a5
Daniel Yee <yeedan@gmail.com>
2014-10-30 17:22:03 -0700

    fix login authneticate

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,7 +21,7 @@ end
 
 post '/login' do
   #authenticate a user
-  @user = User.find(username: params[:username])
+  @user = User.find_by(username: params[:username])
   if @user.authenticate(params[:password])
     session[:user_id] = @user.id
   else
